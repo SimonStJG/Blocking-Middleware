fcb37b7b3089a9dccfb830c00c0ed6042d03a65f
Simon StJG <Simon.StJG@gmail.com>
2017-11-28 21:07:52 +0000

    Add logging and defaulting to refresh_random_links

--- backend/refresh_random_links.php
+++ backend/refresh_random_links.php
@@ -1,35 +1,61 @@
 <?php
 
+/**
+ * Refresh randomlinks and randomcat lists.
+ *
+ * For both lists, if there are fewer than argv[2] elements in the list, then add an additional
+ * arg[1] elements.
+ */
+
 $dir = dirname(__FILE__);
 include "$dir/../api/1.2/libs/DB.php";
 include "$dir/../api/1.2/libs/services.php";
 include "$dir/../api/1.2/libs/amqp.php";
+
+# TODO What are the correct defaults here?
+$DEFAULT_LIST_SIZE_THRESHOLD = 100;
+$DEFAULT_NUM_ADDITIONAL_ITEMS = 200;
+
 $conn = db_connect();
 $redis = redis_connect("cache");
 
 $key = "randomlinks";
-
 $loader = new UrlLoader($conn);
 
-if ($redis->lLen($key) < $argv[2]) {
-
-    $links = $loader->get_unreported_blocks($argv[1]);
-
-    foreach ($links as $url) {
-        $redis->rPush($key, $url);
-    }
-
+if ($argc < 3) {
+    print "Insuffient command line arguments provided, using defaults\n";
+    $list_size_threshold = $DEFAULT_LIST_SIZE_THRESHOLD;
+    $num_additional_items = $DEFAULT_NUM_ADDITIONAL_ITEMS;
+} else {
+    $list_size_threshold = $argv[2];
+    $num_additional_items = $argv[1];
 }
+print "Running with: \n";
+print "  list_size_threshold: $list_size_threshold\n";
+print "  num_additional_items: $num_additional_items\n";
 
-$key = "randomcat";
-$loader = new DMOZCategoryLoader($conn);
+try {
+  print $redis->lLen($key);
+  if ($redis->lLen($key) < $list_size_threshold) {
+      $links = $loader->get_unreported_blocks($num_additional_items);
+      print sprintf("Found %d unreported blocks\n", length($links));
+      foreach ($links as $url) {
+          $redis->rPush($key, $url);
+      }
+  }
 
-if ($redis->lLen($key) < $argv[2]) {
+  $key = "randomcat";
+  $loader = new DMOZCategoryLoader($conn);
 
-    $cats = $loader->random($argv[1]);
-
-    foreach ($cats as $cat) {
-        $redis->rPush($key, $cat);
-    }
-
+  if ($redis->lLen($key) < $list_size_threshold) {
+      $cats = $loader->random($num_additional_items);
+      print sprintf("Found %d random categories\n", length($cats));
+      foreach ($cats as $cat) {
+          $redis->rPush($key, $cat);
+      }
+  }
+} catch (Exception $e) {
+  print "refresh_random_links failed: $e\n";
+  print $e.message;
+  exit(1);
 }
